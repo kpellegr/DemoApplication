e0aa17e1ca0b0e488006363acdb90b247bdce90c
Koen Pellegrims <koen.pellegrims@gmail.com>
2022-02-15 17:28:09 +0100

    Button en dynamisch label toegevoegd

--- app/src/main/java/com/example/demoapplication/MainActivity.java
+++ app/src/main/java/com/example/demoapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.demoapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mijnknop = findViewById(R.id.mijnbutton);
+        mijnknop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView mijntextview = findViewById(R.id.mijntextview);
+                mijntextview.setText("Hallo Koen!");
+            }
+        });
     }
 }
